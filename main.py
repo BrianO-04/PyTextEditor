6a149f02577f2d07be51c02922f32f57c778e603
Brian Olson <brianolson04@gmail.com>
2025-07-20 16:05:49 -0500

    added 'replace all' popup and function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter import *
 
 def save(window, text_edit):
     fp = asksaveasfilename(filetypes=[("Text Files", "*.txt")])
@@ -26,24 +27,66 @@ def load(window, text_edit):
         f.close()
     window.title(f"{fp}")
 
+def replace_window(window, text_edit):
+    global replace_popup
+    replace_popup = Tk()
+    replace_popup.title("Replace All")
+    replace_popup.geometry("500x150")
+    
+    rep_frame = Frame(replace_popup)
+    rep_frame.pack(pady=5)
+
+    buttons_frame = Frame(replace_popup)
+    buttons_frame.pack(pady=5, side=tk.BOTTOM)
+
+    orig_label = Label(rep_frame, text="Original text: ")
+    orig_inp_frame = Frame(rep_frame, width=400, height=50)
+    orig_inp_frame.grid_propagate(False)
+    orig_inp = tk.Text(orig_inp_frame, font="Helvetica 18")
+    orig_inp.grid(row=0, column=0)
+
+    new_label = Label(rep_frame, text="New text: ")
+    new_inp_frame = Frame(rep_frame, width=400, height= 50)
+    new_inp_frame.grid_propagate(False)
+    new_inp = tk.Text(new_inp_frame, font="Helvetica 18")
+    new_inp.grid(row=0, column=0)
+
+    orig_label.grid(row=0, column=0)
+    orig_inp_frame.grid(row=0, column=1)
+
+    new_label.grid(row=1, column=0)
+    new_inp_frame.grid(row=1, column=1)
+
+    confirm = Button(buttons_frame, text="Confirm", command=lambda: replace(window, text_edit, orig_inp.get(1.0, tk.END).strip(), new_inp.get(1.0, tk.END).strip()))
+    cancel = Button(buttons_frame, text="Cancel", command=replace_popup.destroy)
+
+    confirm.grid(row=0, column=0, pady=5, padx=5)
+    cancel.grid(row=0, column=1, pady=5, padx=5)
+
 def replace(window, text_edit, original, replace):
     content = text_edit.get(1.0, tk.END)
     first_index = content.find(original)
 
+    print(original)
+    print(replace)
+
     if first_index == -1:
+        replace_popup.destroy()
         return
 
     new_content = ""
 
     while first_index != -1:
         new_content += content[:first_index] + replace
-        content = content[first_index+1:]
+        content = content[first_index+len(original):]
         first_index = content.find(original)
         if first_index == -1:
             new_content += content
 
     text_edit.delete(1.0, tk.END)
     text_edit.insert(tk.END, new_content)
+
+    replace_popup.destroy()
 
 def main():
     window = tk.Tk()
@@ -58,7 +101,7 @@ def main():
     
     save_button = tk.Button(frame, text="Save", command=lambda: save(window, text_edit))
     open_button = tk.Button(frame, text="Open", command=lambda: load(window, text_edit))
-    replace_button = tk.Button(frame, text="Replace all", command=lambda: replace(window, text_edit, 'a', 'test'))
+    replace_button = tk.Button(frame, text="Replace all", command=lambda: replace_window(window, text_edit))
 
     save_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
     open_button.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
