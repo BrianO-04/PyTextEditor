fe46ca809b8d0bb0a10ea09c5906a78d356914f1
Brian Olson <brianolson04@gmail.com>
2025-08-03 16:55:00 -0500

    removed unnecessary print statement from interpreter

--- interpreter.py
+++ interpreter.py
@@ -16,8 +16,6 @@ def load_script():
     for c in content.split(";"):
         if len(c.strip()) > 0:
             commands.append(c.strip())
-    
-    print(commands)
 
     for command in commands:
         if len(command) > 0:
@@ -43,23 +41,26 @@ def run_command(command):
     
 def parse_command(command):
     index = 0
+    fst_index = 0
 
     sub_commands = []
 
-    start_index = 0
-
+    parens_index = 0 #tracks the index of the current parentheses
     open_parens = 0
     close_parens = 0
 
     for char in command:
-        if char == '(':
+        if char == ' ' and open_parens == 0:
+            sub_commands.append(command[fst_index:index])
+            fst_index = index+1
+        elif char == '(':
             if open_parens == 0:
-                start_index = index
+                parens_index = index
             open_parens += 1
         elif char == ')':
             close_parens += 1
             if open_parens == close_parens:
-                sub_commands.append(command[start_index+1:index])
+                sub_commands.append(command[parens_index+1:index])
                 open_parens = 0
                 close_parens = 0
         index += 1
