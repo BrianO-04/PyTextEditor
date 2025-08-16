b0450eeb19024a2ecf81da556f7079cce0b87157
Brian Olson <brianolson04@gmail.com>
2025-08-16 13:26:49 -0500

    added representation to token type

--- tokens.py
+++ tokens.py
@@ -12,3 +12,6 @@ class TokenType(Enum):
 class Token:
     type: TokenType
     value: any = None
+
+    def __repr__(self):
+        return self.type.name + (f": {self.value}" if self.value != None else "")
